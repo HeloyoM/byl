d70a2062293e4010c42a2c49667d8da24eddf1e3
unknown <mybs2323@gmail.com>
2025-02-24 22:51:42 +0200

    setup cloudinary wip

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -10,29 +10,128 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
+  secure: true
 });
+
+console.log(cloudinary.config());
+
+/////////////////////////
+// Uploads an image file
+/////////////////////////
+// const uploadImage = async (imagePath) => {
+
+  // Use the uploaded file's name as the asset's public ID and 
+  // allow overwriting the asset with new versions
+//   const options = {
+//     use_filename: true,
+//     unique_filename: false,
+//     overwrite: true,
+//   };
+
+//   try {
+//     // Upload the image
+//     const result = await cloudinary.uploader.upload(imagePath, options);
+//     console.log(result);
+//     return result.public_id;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+
+/////////////////////////////////////
+// Gets details of an uploaded image
+/////////////////////////////////////
+// const getAssetInfo = async (publicId) => {
+
+//   // Return colors in the response
+//   const options = {
+//     colors: true,
+//   };
+
+//   try {
+//     // Get details about the asset
+//     const result = await cloudinary.api.resource(publicId, options);
+//     console.log(result);
+//     return result.colors;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+
+//////////////////////////////////////////////////////////////
+// Creates an HTML image tag with a transformation that
+// results in a circular thumbnail crop of the image  
+// focused on the faces, applying an outline of the  
+// first color, and setting a background of the second color.
+//////////////////////////////////////////////////////////////
+// const createImageTag = (publicId, ...colors) => {
+
+//   // Set the effect color and background color
+//   const [effectColor, backgroundColor] = colors;
+
+//   // Create an image tag with transformations applied to the src URL
+//   let imageTag = cloudinary.image(publicId, {
+//     transformation: [
+//       { width: 250, height: 250, gravity: 'faces', crop: 'thumb' },
+//       { radius: 'max' },
+//       { effect: 'outline:10', color: effectColor },
+//       { background: backgroundColor },
+//     ],
+//   });
+
+//   return imageTag;
+// };
+
+
+
+//////////////////
+//
+// Main function
+//
+//////////////////
+// (async () => {
+
+//   // Set the image to upload
+//   const imagePath = 'https://cloudinary-devs.github.io/cld-docs-assets/assets/images/happy_people.jpg';
+
+//   // Upload the image
+//   const publicId = await uploadImage(imagePath);
+//   console.log({ publicId })
+//   // Get the colors in the image
+//   const colors = await getAssetInfo(publicId);
+
+//   // Create an image tag, using two of the colors in a transformation
+//   const imageTag = await createImageTag(publicId, colors[0][0], colors[1][0]);
+
+//   // Log the image tag to the console
+//   console.log(imageTag);
+
+// })();
+
 
 // Upload media
-router.post("/upload", async (req, res) => {
-  try {
-    const { type, description, file } = req.body; // You may want to handle file uploads using a middleware (e.g., multer)
-    
-    // Upload to Cloudinary
-    const result = await cloudinary.uploader.upload(file, { resource_type: type });
+// router.post("/upload", async (req, res) => {
+//   try {
+//     const { type, description, file } = req.body; // You may want to handle file uploads using a middleware (e.g., multer)
 
-    const newMedia = new Media({
-      url: result.secure_url,
-      type: type,
-      description: description,
-      uploadedBy: req.user._id, // Assuming user is authenticated
-    });
+//     // Upload to Cloudinary
+//     const result = await cloudinary.uploader.upload(file, { resource_type: type });
 
-    await newMedia.save();
-    res.status(200).json(newMedia);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+//     const newMedia = new Media({
+//       url: result.secure_url,
+//       type: type,
+//       description: description,
+//       uploadedBy: req.user._id, // Assuming user is authenticated
+//     });
+
+//     await newMedia.save();
+//     res.status(200).json(newMedia);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 // Delete media
 router.delete("/delete/:id", async (req, res) => {
@@ -42,8 +141,19 @@ router.delete("/delete/:id", async (req, res) => {
 
     // Optionally, delete from Cloudinary as well
     await cloudinary.uploader.destroy(media.public_id);
-    
+
     res.status(200).json({ message: "Media deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//get resources
+router.get("/resources", async (req, res) => {
+  try {
+    console.log({ cloudinary })
+    const media = await cloudinary.api.resource().then(result => console.log({ result }))
+    console.log({ media })
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
